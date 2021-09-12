30eca8378fd31e23519d652f1fb96fd61378089e
Mark Bush <mark@bushnet.org>
2021-09-12 09:50:23 +0100

    SBC ZP

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -913,7 +913,9 @@ class CPU6502 : Chip {
       []
     ],
     [ // e5 SBC ZP
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_AD_to_ADDR_B], // Read Arg
+      [.I_SBC, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Subtract from A, Next OP
     ],
     [ // e6 INC ZP
       []
