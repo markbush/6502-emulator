86583d7d9f9a8e62ea0243da3df352df8c95acef
Mark Bush <mark@bushnet.org>
2021-09-12 11:02:02 +0100

    EOR Abs,Y

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -486,7 +486,11 @@ class CPU6502 : Chip {
       [.I_CLI, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Clear interrupt, Next OP
     ],
     [ // 59 EOR Abs,Y
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_ADL_plus_Y, .I_PC_to_ADDR_B, .I_PC_INCR], // Read ADH, ADL+Y
+      [.I_DATA_to_ADH, .I_CHK_carry, .I_AD_to_ADDR_B], // Read Arg - discard if carry
+      [.I_ADH_INCR, .I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_EOR, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Eor to A, Next OP
     ],
     [ // 5a
       []
