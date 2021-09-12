8f0f3210ca70298fbefb7a5b6d844d56e4c77f77
Mark Bush <mark@bushnet.org>
2021-09-12 11:24:10 +0100

    ORA ZP,X

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -254,7 +254,10 @@ class CPU6502 : Chip {
       []
     ],
     [ // 15 ORA ZP,X
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_AD_to_ADDR_B, .I_ADL_plus_X], // Read arg, ADL+X
+      [.I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_ORA, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Or to A, Next OP
     ],
     [ // 16 ASL ZP,X
       []
