b6bc3018042b692f0c27ec1a6c354f4ef59ac3f2
Mark Bush <mark@bushnet.org>
2021-09-12 11:06:23 +0100

    EOR ZP

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -422,7 +422,9 @@ class CPU6502 : Chip {
       []
     ],
     [ // 45 EOR ZP
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_AD_to_ADDR_B], // Read Arg
+      [.I_EOR, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Eor to A, Next OP
     ],
     [ // 46 LSR ZP
       []
