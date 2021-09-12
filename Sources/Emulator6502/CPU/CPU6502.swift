fdce679dd85f190cef993141c94390562fdc94e8
Mark Bush <mark@bushnet.org>
2021-09-12 11:51:51 +0100

    CMP Abs

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -923,7 +923,10 @@ class CPU6502 : Chip {
       []
     ],
     [ // cd CMP Abs
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_PC_to_ADDR_B, .I_PC_INCR], // Read ADH
+      [.I_DATA_to_ADH, .I_AD_to_ADDR_B], // Read Arg
+      [.I_CMP, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Compare with A, Next OP
     ],
     [ // ce DEC Abs
       []
