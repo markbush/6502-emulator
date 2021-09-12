285d0eb349048ab0cbcaf9046eba64002c3a7f6e
Mark Bush <mark@bushnet.org>
2021-09-12 11:56:46 +0100

    CMP Abs,Y

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -963,7 +963,11 @@ class CPU6502 : Chip {
       [.I_CLD, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Clear decimal, Next OP
     ],
     [ // d9 CMP Abs,Y
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_ADL_plus_Y, .I_PC_to_ADDR_B, .I_PC_INCR], // Read ADH, ADL+Y
+      [.I_DATA_to_ADH, .I_CHK_carry, .I_AD_to_ADDR_B], // Read Arg - discard if carry
+      [.I_ADH_INCR, .I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_CMP, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Compare with A, Next OP
     ],
     [ // da
       []
