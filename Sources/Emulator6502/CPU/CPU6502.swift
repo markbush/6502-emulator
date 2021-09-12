531b3bc89ded25dd4dd4c64279217907f66a1853
Mark Bush <mark@bushnet.org>
2021-09-12 13:28:06 +0100

    LDA ZP Ind Y

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -846,7 +846,12 @@ class CPU6502 : Chip {
       []
     ],
     [ // b1 LDA (ZP),Y
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for IAL)
+      [.I_DATA_to_ADL, .I_AD_to_ADDR_B, .I_ADL_INCR], // Read 00,IAL (for BAL), IAL=IAL+1
+      [.I_AD_to_ADDR_B, .I_DATA_to_ADL, .I_ADL_plus_Y], // Read 00,IAL+1 (for BAH), save BAL+Y
+      [.I_DATA_to_ADH, .I_CHK_carry, .I_AD_to_ADDR_B], // Read arg - discard if carry
+      [.I_ADH_INCR, .I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_DATA_to_A, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Load A, Next OP
     ],
     [ // b2
       []
