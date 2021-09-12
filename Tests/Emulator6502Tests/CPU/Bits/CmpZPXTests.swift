a645941082a2a89379cc9cd63834d249394d3498
Mark Bush <mark@bushnet.org>
2021-09-12 12:02:51 +0100

    CMP ZP,X

--- Tests/Emulator6502Tests/CPU/Bits/CmpZPXTests.swift
+++ Tests/Emulator6502Tests/CPU/Bits/CmpZPXTests.swift
@@ -23,6 +23,8 @@ final class CmpZpXTests: XCTestCase {
 
     TestHelper.startupSequence(cpu: cpu, pins: pins, mem: memory)
     cpu.a.value = testValue1 // Set the accumulator
+    // Clear carry, set zero, negative
+    cpu.status.value = Status6502.ZERO | Status6502.NEGATIVE
 
     // Next instruction should be op at RESET address
     XCTAssertEqual(pins.address.value, TestHelper.RES_ADDR)
@@ -42,9 +44,12 @@ final class CmpZpXTests: XCTestCase {
     TestHelper.cycle(cpu, pins: pins, mem: memory)
     XCTAssertEqual(pins.data.value, testValue2)
 
-    // Add arg to A
+    // Cmp arg to A
     TestHelper.cycle(cpu, pins: pins, mem: memory)
-    XCTAssertEqual(cpu.a.value, testValue1 &+ testValue2)
+    XCTAssertEqual(cpu.a.value, testValue1)
+    XCTAssert(cpu.status.carry)
+    XCTAssertFalse(cpu.status.zero)
+    XCTAssertFalse(cpu.status.negative)
 
     // Decode NOP
     TestHelper.cycle(cpu, pins: pins, mem: memory)
@@ -71,6 +76,8 @@ final class CmpZpXTests: XCTestCase {
 
     TestHelper.startupSequence(cpu: cpu, pins: pins, mem: memory)
     cpu.a.value = testValue1 // Set the accumulator
+    // Clear carry, set zero, negative
+    cpu.status.value = Status6502.ZERO | Status6502.NEGATIVE
 
     // Next instruction should be op at RESET address
     XCTAssertEqual(pins.address.value, TestHelper.RES_ADDR)
@@ -90,9 +97,12 @@ final class CmpZpXTests: XCTestCase {
     TestHelper.cycle(cpu, pins: pins, mem: memory)
     XCTAssertEqual(pins.data.value, testValue2)
 
-    // Add arg to A
+    // Cmp arg to A
     TestHelper.cycle(cpu, pins: pins, mem: memory)
-    XCTAssertEqual(cpu.a.value, testValue1 &+ testValue2)
+    XCTAssertEqual(cpu.a.value, testValue1)
+    XCTAssert(cpu.status.carry)
+    XCTAssertFalse(cpu.status.zero)
+    XCTAssertFalse(cpu.status.negative)
 
     // Decode NOP
     TestHelper.cycle(cpu, pins: pins, mem: memory)
