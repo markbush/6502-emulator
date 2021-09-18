8dc95f4951d8c4560b3cca3dcfeff8452071f7be
Mark Bush <mark@bushnet.org>
2021-09-18 18:19:02 +0100

    LSR Abs,X

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -596,7 +596,13 @@ class CPU6502 : Chip {
       [.I_EOR, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Eor to A, Next OP
     ],
     [ // 5e LSR Abs,X
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_ADL_plus_X, .I_PC_to_ADDR_B, .I_PC_INCR], // Read ADH, ADL+X
+      [.I_DATA_to_ADH, .I_AD_to_ADDR_B], // Read Arg - discarded
+      [.I_ADH_INCR, .I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_AD_to_ADDR_B, .I_WRITE], // Shift A right
+      [.I_LSR, .I_AD_to_ADDR_B, .I_WRITE], // Write correct value
+      [.I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Next OP
     ],
     [ // 5f
       []
---
+++ Tests/Emulator6502Tests/CPU/Shift/LsrAbsXTests.swift
@@ -0,0 +1,68 @@
+import XCTest
+import Foundation
+@testable import Emulator6502
+
+final class LsrAbsXTests: XCTestCase {
+  func testLsrAbsX() {
+    print("debug: testLsrAbsX")
+    let pins = Pins()
+    let testValue1:UInt8 = 0xc3
+    let testValue2:UInt8 = 0x26
+    let memory = TestHelper.initMemory(pins)
+    let memStore:UInt16 = 0x5a3c
+    let offset:UInt8 = 0x05
+    let actualStore:UInt16 = 0x5a41
+    // First OP after reset is op
+    memory[TestHelper.RES_ADDR] = TestHelper.LSRAbsX
+    memory[TestHelper.RES_ADDR&+1] = UInt8(memStore & 0xff) // low byte
+    memory[TestHelper.RES_ADDR&+2] = UInt8(memStore >> 8) // high byte
+    memory[TestHelper.RES_ADDR&+3] = TestHelper.NOP
+    memory[actualStore] = testValue2
+    let cpu = CPU6502(pins)
+    cpu.reset()
+    cpu.x.value = offset // Index offset from base address
+
+    TestHelper.startupSequence(cpu: cpu, pins: pins, mem: memory)
+    cpu.a.value = testValue1 // Set the accumulator
+
+    // Next instruction should be op at RESET address
+    XCTAssertEqual(pins.address.value, TestHelper.RES_ADDR)
+    XCTAssertEqual(pins.data.value, TestHelper.LSRAbsX)
+
+    print("debug: perform LSR")
+    // decode OP - fetch ADL
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.ir.value, TestHelper.LSRAbsX)
+
+    // Save ADL - fetch arg, discarded
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+
+    // Save ADL - fetch ADH
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(memory[actualStore], testValue2)
+
+    // Save ADH - fetch arg
+    // Memory unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(pins.data.value, testValue2)
+    XCTAssertEqual(memory[actualStore], testValue2)
+
+    // Shift data
+    // A unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.a.value, testValue1)
+
+    // Save data
+    // A unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.a.value, testValue1)
+    XCTAssertEqual(memory[actualStore], testValue2 >> 1)
+
+    // Fetch next op
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+
+    // Decode NOP
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.ir.value, TestHelper.NOP)
+  }
+}
