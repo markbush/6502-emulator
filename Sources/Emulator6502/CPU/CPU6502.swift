4a909a4daf5df5f078c3ed44bf2a14b548b2f4e7
Mark Bush <mark@bushnet.org>
2021-09-20 15:54:54 +0100

    INC Zp,X

--- Sources/Emulator6502/CPU/CPU6502.swift
+++ Sources/Emulator6502/CPU/CPU6502.swift
@@ -1408,7 +1408,12 @@ class CPU6502 : Chip {
       [.I_SBC, .I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Subtract from A, Next OP
     ],
     [ // f6 INC ZP,X
-      []
+      [.I_PC_to_ADDR_B, .I_PC_INCR], // Read PC (for ADL)
+      [.I_DATA_to_ADL, .I_AD_to_ADDR_B, .I_ADL_plus_X], // Read arg, ADL+X
+      [.I_AD_to_ADDR_B], // Read arg from adjusted address
+      [.I_AD_to_ADDR_B, .I_WRITE], // Increment data
+      [.I_INC, .I_AD_to_ADDR_B, .I_WRITE], // Write correct value
+      [.I_PC_to_ADDR_B, .I_NEXT_OP, .I_PC_INCR] // Next OP
     ],
     [ // f7
       []
---
+++ Tests/Emulator6502Tests/CPU/Arithmetic/IncZpXTests.swift
@@ -0,0 +1,63 @@
+import XCTest
+import Foundation
+@testable import Emulator6502
+
+final class IncZpXTests: XCTestCase {
+  func testIncZpX() {
+    print("debug: testIncZpX")
+    let pins = Pins()
+    let testValue1:UInt8 = 0xc3
+    let testValue2:UInt8 = 0x26
+    let memory = TestHelper.initMemory(pins)
+    let memStore:UInt16 = 0x3c
+    let offset:UInt8 = 0x05
+    let actualStore:UInt16 = 0x41
+    // First OP after reset is op
+    memory[TestHelper.RES_ADDR] = TestHelper.INCZpX
+    memory[TestHelper.RES_ADDR&+1] = UInt8(memStore & 0xff) // low byte
+    memory[TestHelper.RES_ADDR&+2] = TestHelper.NOP
+    memory[actualStore] = testValue2
+    let cpu = CPU6502(pins)
+    cpu.reset()
+    cpu.x.value = offset // Index offset from base address
+
+    TestHelper.startupSequence(cpu: cpu, pins: pins, mem: memory)
+    cpu.a.value = testValue1 // Set the accumulator
+
+    // Next instruction should be op at RESET address
+    XCTAssertEqual(pins.address.value, TestHelper.RES_ADDR)
+    XCTAssertEqual(pins.data.value, TestHelper.INCZpX)
+
+    print("debug: perform INC")
+    // decode OP - fetch ADL
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.ir.value, TestHelper.INCZpX)
+
+    // Save ADL - fetch arg, discarded
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+
+    // Fetch arg
+    // Memory unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(pins.data.value, testValue2)
+    XCTAssertEqual(memory[actualStore], testValue2)
+
+    // Inc data
+    // A unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.a.value, testValue1)
+
+    // Save data
+    // A unchanged
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.a.value, testValue1)
+    XCTAssertEqual(memory[actualStore], testValue2 &+ 1)
+
+    // Fetch next op
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+
+    // Incode NOP
+    TestHelper.cycle(cpu, pins: pins, mem: memory)
+    XCTAssertEqual(cpu.ir.value, TestHelper.NOP)
+  }
+}
